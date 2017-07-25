ff9cf517ba32cc396a1f5b187df156b856641e35
Gina De La Rosa <g26delarosa@gmail.com>
2017-07-25 09:05:40 -0700

    finished UI

--- DropSee/Controller/MapVC.swift
+++ DropSee/Controller/MapVC.swift
@@ -7,19 +7,26 @@
 //
 
 import UIKit
+import MapKit
 
 class MapVC: UIViewController {
-
+    
+    @IBOutlet var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mapView.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+
+    @IBAction func centerMapButton(_ sender: Any) {
+        
     }
-
-
+    
 }
 
+extension MapVC: MKMapViewDelegate {
+    
+}
